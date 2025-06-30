f32ea4ec221c16765b5232c704796f580a2d92c0
mdnazmulhasanniloy <mdnazmulhasanniloy323@gmail.com>
2025-07-01 01:57:35 +0600

    fix: eslint problem

--- src/redux/features/user/usersApi.ts
+++ src/redux/features/user/usersApi.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+ 
 import { tagTypes } from "@/redux/tagTypes";
 import { baseApi } from "../../api/baseApi";
 import { IApiResponseWithMeta, IUser, IQueryParams, IMyProfile } from "@/types";
